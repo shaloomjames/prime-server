a1bb98b24f50175839fc209e6401b2a00da55277
shaloomjames <151752846+shaloomjames@users.noreply.github.com>
2024-11-19 04:12:22 +0500

    Update Db.js
    put the connection string directly

--- config/Db.js
+++ config/Db.js
@@ -3,7 +3,7 @@
 
     const ConnectDB = async()=>{
         try {
-            const conn = await mongoose.connect(process.env.MongoDB_Connection_String);
+            const conn = await mongoose.connect("mongodb://admin:admin@clustervertex-shard-00-00.fr2hb.mongodb.net:27017,clustervertex-shard-00-01.fr2hb.mongodb.net:27017,clustervertex-shard-00-02.fr2hb.mongodb.net:27017/ExpanceDb?ssl=true&replicaSet=atlas-rj523g-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ClusterVertex");
             console.log(`server is connected to ${conn.connection.db.databaseName} database`)
         } catch (error) {
             console.log(`Db Connection Error ${error}`)
